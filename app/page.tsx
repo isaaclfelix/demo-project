7db0ccaf4ff9502ef736d8a63600b4bad39107e7
isaaclfelix <isaaclfelix@gmail.com>
2026-05-07 18:03:04 -0600

    feat: remove wrapper test

--- app/page.tsx
+++ app/page.tsx
@@ -11,40 +11,38 @@ export default function Home() {
     <div>
       <Navigation />
       <main>
-        <Hero.Wrapper className="bg-background">
-          <Hero.Content
-            title={
-              <>
-                Next.js <br />+<br /> Convex
-              </>
-            }
-            body={
-              <>
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
-              </>
-            }
-            actions={
-              <ButtonGroup>
-                <Button size="lg" asChild>
-                  <Link href="/blog">Blog</Link>
-                </Button>
-                <Button size="lg" variant="outline" asChild>
-                  <Link href="/docs">Docs</Link>
-                </Button>
-              </ButtonGroup>
-            }
-            image={{
-              src: HeroImage,
-              alt: "Next.js + WordPress",
-              width: 600,
-              height: 600,
-              priority: true,
-              fetchPriority: "high",
-              style: { objectFit: "cover" },
-            }}
-          />
-        </Hero.Wrapper>
+        <Hero
+          title={
+            <>
+              Next.js <br />+<br /> Convex
+            </>
+          }
+          body={
+            <>
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+            </>
+          }
+          actions={
+            <ButtonGroup>
+              <Button size="lg" asChild>
+                <Link href="/blog">Blog</Link>
+              </Button>
+              <Button size="lg" variant="outline" asChild>
+                <Link href="/docs">Docs</Link>
+              </Button>
+            </ButtonGroup>
+          }
+          image={{
+            src: HeroImage,
+            alt: "Next.js + WordPress",
+            width: 600,
+            height: 600,
+            priority: true,
+            fetchPriority: "high",
+            style: { objectFit: "cover" },
+          }}
+        />
       </main>
     </div>
   );
