8fb8ad6f987ad25170c6b27a97e794c969827106
isaaclfelix <isaaclfelix@gmail.com>
2026-05-08 20:43:41 -0600

    feat: update schema for verification code

--- lib/schemas/verify.ts
+++ lib/schemas/verify.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const verifySchema = z.object({
-  code: z.string(),
+  code: z.string().min(6).max(6),
 });
 
 export type VerifySchema = z.infer<typeof verifySchema>;
