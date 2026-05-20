ae36b3294c1b5d8cebd8638a3a722fd20db1f6d6
isaaclfelix <isaaclfelix@gmail.com>
2026-05-19 20:17:36 -0600

    fix: error with type import path

--- lib/content/cachedGetPost.ts
+++ lib/content/cachedGetPost.ts
@@ -4,8 +4,7 @@ import { fetchQuery } from "convex/nextjs";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-
-import { PostWithParsedContent } from "../schemas/convex";
+import { PostWithParsedContent } from "@/convex/posts";
 
 export const cachedGetPost = cache(
   async (postId: string): Promise<PostWithParsedContent | Error> => {
